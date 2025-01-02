c3c98e34c5b312b7aff9f89bbf9827d9bc06837b
Dimitris Zervas <dzervas@dzervas.gr>
2025-01-02 16:35:46 +0200

    Add some commands and pipfile
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

--- APIParser.py
+++ APIParser.py
@@ -9,7 +9,65 @@ def parseMail(primitive, params):
                 f"Link Date: {params[7]:02}/{params[6]:02}/{params[5]:02} at {params[8]:02}:{params[9]:02}"
             )
             # Mode can be changed with an API_PROD_TEST_REQ command
-            print(f"Dect mode: {params[10]}")
+            # print(f"Dect mode: {params[10]}")
+            dect_mode_id = params[10]
+            dect_mode = ""
+
+            if dect_mode_id == 0:
+                dect_mode = "EU"
+            elif dect_mode_id == 1:
+                dect_mode = "US"
+            elif dect_mode_id == 2:
+                dect_mode = "SA"
+            elif dect_mode_id == 3:
+                dect_mode = "Taiwan"
+            elif dect_mode_id == 4:
+                dect_mode = "Malaysia"
+            elif dect_mode_id == 5:
+                dect_mode = "China"
+            elif dect_mode_id == 6:
+                dect_mode = "Thailand"
+            elif dect_mode_id == 7:
+                dect_mode = "Brazil"
+            elif dect_mode_id == 8:
+                dect_mode = "US Extended"
+            elif dect_mode_id == 9:
+                dect_mode = "Korea"
+            elif dect_mode_id == 10:
+                dect_mode = "Japan (2ch)"
+            elif dect_mode_id == 11:
+                dect_mode = "Japan (5ch)"
+            else:
+                dect_mode = "Invalid"
+
+            print(f"DECT mode: {dect_mode}")
+        case 0x4005:
+            print("API_FP_MM_GET_ID_CFM received.")
+            print(f"ID: {params[1]:02x}{params[2]:02x}{params[3]:02x}{params[4]:02x}")
+        case 0x400B:
+            print("API_FP_MM_GET_ACCESS_CODE_CFM received.")
+            access_code = (
+                f"{params[1]:02x}{params[2]:02x}{params[3]:02x}{params[4]:02x}"
+            )
+            access_code = access_code.lstrip("f")
+            print(f"Access Code: {access_code}")
+        case 0x4106:
+            print(
+                "API_FP_MM_SET_REGISTRATION_MODE_CFM received:",
+                "Success" if params[0] == 0 else f"Error: {params[0]}",
+            )
+        case 0x4107:
+            print("API_FP_MM_REGISTRATION_COMPLETE_IND received.")
+            print("Registration complete!")
+            print("Handset ID", params[1])
+            # print(f"resp len {params[2]:02x} {params[3]:02x}")
+            # length = int(params[2:3])
+            # print("InfoElement", params[4 : 4 + length])
+        case 0x4108:
+            print("API_FP_MM_HANDSET_PRESENT_IND received.")
+            print("New handset present!")
+            print("Handset ID", params[0])
+
         case 0x5903:
             print("API_HAL_LED_CFM  received.")
             print("LEDs toggled.")
--- main.py
+++ main.py
@@ -18,12 +18,44 @@ async def main():
     asyncio.create_task(protocol.poll_timer())
 
     await asyncio.sleep(2)
+
+    # Uncomment to set region
+    # print("Sending 'API_PROD_TEST_REQ' request command...")
+    # protocol.send_command(
+    #     program_id=0,
+    #     task_id=1,
+    #     primitive=0x4FFE,  # API_PROD_TEST_REQ
+    #     params=[
+    #         0x00,
+    #         0x02,  # SET_DECT_MODE
+    #         0x00,
+    #         0x01,  # Parameter len
+    #         0x00,  # EU
+    #     ],
+    # )
+
     print("Sending 'API_FP_GET_FW_VERSION' request command...")
 
     protocol.send_command(
         program_id=0,
         task_id=1,
         primitive=0x4002,
+        params=[],
+    )
+    print("Sending 'API_FP_MM_GET_ID_REQ' request command...")
+
+    protocol.send_command(
+        program_id=0,
+        task_id=1,
+        primitive=0x4004,
+        params=[],
+    )
+    print("Sending 'API_FP_MM_GET_ACCESS_CODE_REQ' request command...")
+
+    protocol.send_command(
+        program_id=0,
+        task_id=1,
+        primitive=0x400A,
         params=[],
     )
     print("Sending 'API_HAL_LED_REQ' request command...")
@@ -33,6 +65,19 @@ async def main():
         task_id=1,
         primitive=0x5902,
         params=[0x02, 0x03, 0x01, 0x2C, 0x01, 0x00, 0x2C, 0x01, 0x02, 0x0A, 0x00],
+    )
+
+    await asyncio.sleep(2)
+    print("==== Entering registration mode ====")
+
+    protocol.send_command(
+        program_id=0,
+        task_id=1,
+        primitive=0x4105,  # API_FP_MM_SET_REGISTRATION_MODE_REQ
+        params=[
+            2,  # Enable registration until a handset is registered
+            1,  # DeleteLastHandset (if max handsets reached)
+        ],
     )
 
     await asyncio.Future()
